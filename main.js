17c9734ee9dbf005276edffb028d8b212debf1ec
Harvey Hutchings-Smith <harveyhutchingssmith@gmail.com>
2015-04-24 20:41:59 +1000

    Added meme

--- main.js
+++ main.js
@@ -62,5 +62,32 @@ function run()
 	//draw FPS
 	context.fillStyle = "#f00";
 	context.font = "14px Arial";
-	context.fillStyle("FPS: " + fps, 5, 20, 100);
+	context.fillText("FPS: " + fps, 5, 20, 100);
 }
+
+//-------------------- Don't modify anything below here
+// This code will set up the framework so that the 'run' function is
+// called 60 times per second. We have some options to fall back on
+// in case the browser doesn't support our preferred method.
+(function() {
+ var onEachFrame;
+ if (window.requestAnimationFrame) {
+ onEachFrame = function(cb) {
+ var _cb = function() { cb(); window.requestAnimationFrame(_cb); }
+ _cb();
+ };
+ } else if (window.mozRequestAnimationFrame) {
+ onEachFrame = function(cb) {
+ var _cb = function() { cb();
+window.mozRequestAnimationFrame(_cb); }
+ _cb();
+ };
+ } else {
+ onEachFrame = function(cb) {
+ setInterval(cb, 1000 / 60);
+ }
+ }
+
+ window.onEachFrame = onEachFrame;
+})();
+window.onEachFrame(run);
